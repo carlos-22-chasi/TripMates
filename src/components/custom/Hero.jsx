ed23ff74122feceb5ed0714aa5cf6b35d9a08a4e
carlos-22-chasi <c071503s@gmail.com>
2024-12-17 19:54:12 -0600

    updated title

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -7,15 +7,24 @@ function Hero() {
     <div className='flex flex-col items-center justify-center mx-10 sm:mx-56 gap-9 pb-20'>
       {/* Introduction Text */}
       <h1 className='font-extrabold md:text-[60px] text-[primary-500] text-center mt-10'>
-        <span className='text-primary'>Your AI Travel Campanion: </span> 
+        <span className='text-primary'>Your AI Travel Campanion: </span>
         Effortless and Personalized Itineraries
       </h1>
       <p className='text-gray-500 text-center'>Your AI travel concierge curates personalized itineraries tailored to your interests and budget, ensuring a seamless and unforgettable adventure</p>
-      
+
       {/* Get Started Button */}
       <Link to={'/create-trip'}>
         <Button className='hover:bg-white hover:text-primary'>Get Started</Button>
       </Link>
+
+      {/* Plane Animation */}
+      <div className="absolute top-100 md:top-52 left-0 w-full h-10 z-0">
+        <img
+          src="/plane2.png"
+          alt="Flying Plane"
+          className="w-24 animate-fly-plane"
+        />
+      </div>
 
       {/* Front Image */}
       <img src='/front.png' className='mt-4 md:mt-20 w-full'></img>
